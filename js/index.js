bd2b7770ad8a534d42d78e493c1ee7cf161b829a
doctordickbaker <doctordickbaker@gmail.com>
2017-02-23 05:03:32 -0600

    Play button inactive animation and initial implementation of Gonzo
    atm Gonzo is nothing more than a simple display that reads: "Currently
    Playing",  but soon he'll have more functionality...

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 var background = document.getElementById("background");
 var but_play = document.getElementById("but_play");
 var audioPlayer = document.getElementById("audioPlayer");
+var gonzo = document.getElementById("gonzo");
 audioPlayer.src = "https://mopidy.electrostat.xyz/stream/live.mp3";
 ///set station's URL here
 
@@ -9,16 +10,14 @@ function Pause() {
     audioPlayer.play();
     but_play.innerHTML = "Pause";
     background.className = "on";
-    ///background.style.margin = "-100vh -100vw";  /// top right bottom left
-    ///background.style.transform = "translate(-108vw,-108vh)";
-    ///background.style.webkitFilter = "blur(8px)";
+    but_play.className = "on";
+    gonzo.className = "on";
   } else {
     audioPlayer.pause();
     but_play.innerHTML = "Play";
     background.className = "off";
-    //background.style.margin = "0vh 0vw 0vh 0vw";
-    //background.style.webkitFilter = "blur(0px)";
-    //background.style.transform = "translate(0vw,-18vh)";
+    but_play.className = "off";
+    gonzo.className = "off";
   }
 }
 
