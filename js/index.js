2b28a37bdea13fd8b4aae8bb35816538842bd3d3
doctordickbaker <doctordickbaker@gmail.com>
2017-02-23 16:23:14 -0600

    Animations.  Animations Galore.   Plus click fix.
    Added animations and fixed a minor issue where the corner of buttons
    wouldn't trigger the function.
    
    The error turned out to be onclick triggering on release,  wihle the
    animation triggered when ::active.
    Swaping onclick (in js) for onmousedown resolved the issue.

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@ var background = document.getElementById("background");
 var but_play = document.getElementById("but_play");
 var audioPlayer = document.getElementById("audioPlayer");
 var gonzo = document.getElementById("gonzo");
+var logo =  document.getElementById("logo");
 audioPlayer.src = "https://mopidy.electrostat.xyz/stream/live.mp3";
 ///set station's URL here
 
@@ -11,16 +12,18 @@ function Pause() {
     but_play.innerHTML = "Pause";
     background.className = "on";
     but_play.className = "on";
+    logo.className = "on";
     gonzo.className = "on";
   } else {
     audioPlayer.pause();
     but_play.innerHTML = "Play";
     background.className = "off";
+    logo.className = "off";
     but_play.className = "off";
     gonzo.className = "off";
   }
 }
 
-but_play.onclick = (function(){
+but_play.onmousedown = (function(){
   Pause();
 })
